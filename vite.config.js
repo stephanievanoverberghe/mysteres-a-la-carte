efa0c040e7140e0f3b2cb2fc1d9e0caa46aa05e3
Stéphanie Vanoverberghe <orangestreet59123@gmail.com>
2024-12-14 16:37:55 +0100

    fix: change path

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
         outDir: '../dist',
         emptyOutDir: true,
     },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import './src/scss/index.scss';`,
+            },
+        },
+    },
     server: {
         port: 3000,
         open: true,
